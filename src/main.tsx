049436e44875c5b78d30ed43ed6f321f93246f30
gustavo silva <guga.b.139935@gmail.com>
2026-05-10 10:06:23 -0400

    Fix: resolve ReferenceError React not defined in image editor

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
-import {StrictMode} from 'react';
+import React, {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+
+// Fix for libraries that expect React to be globally available
+(window as any).React = React;
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
